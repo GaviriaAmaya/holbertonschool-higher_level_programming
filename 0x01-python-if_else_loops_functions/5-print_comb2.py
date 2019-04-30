3229c5fcb9f92d0b374467e0adcad03245465a5f
GaviriaAmaya <judamayaga@unal.edu.co>
2019-04-30 23:16:29 +0000

    [Ft] Print numbers from 00 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if(
-        print("0{:d}, ".format(i), end="")
-    
-#for j in range(0, 100):
-#    print("{:d}, ".format(j), end="")
+    if(i != 99):
+        print("{:02d}, ".format(i), end="")
+    else:
+        print("{:02d}".format(i))
