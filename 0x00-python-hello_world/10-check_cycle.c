db4d0eaa27d1f0ca410dbc654646aeec50a8b32f
GaviriaAmaya <judamayaga@unal.edu.co>
2019-04-30 05:43:32 +0000

    [Addition] Add 3 last exercises

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ *check_cycle - Proves if a list have a cycle
+ *@list: List received
+ *Return: If there's a cycle 1, else 0
+ */
+int check_cycle(listint_t *list)
+{
+listint_t *i = list;
+listint_t *j = list;
+
+if(list != NULL)
+	{
+	j = j->next;
+	while(i != NULL && j != NULL && j->next != NULL)
+		{
+		if (i == j)
+			return(1);
+		i->next;
+		j->next->next;
+		}
+	}
+	return(0);
+}
