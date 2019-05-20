f8bcbfb7d4a9c5240d48d290a113f43777c17616
GaviriaAmaya <judamayaga@unal.edu.co>
2019-05-20 19:37:08 +0000

    [Fix] Adding a counter that returns on any case

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 def safe_print_list(my_list=[], x=0):
+    z = 0
     for y in range(x):
         try:
             print(my_list[y], end="")
+            z += 1
         except IndexError:
-            print()
-            return(y)
+                print()
+                return(z)
     print()
-    return(y + 1)
+    return(z)
