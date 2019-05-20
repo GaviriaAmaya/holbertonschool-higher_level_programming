a1b1393f53ed8b0cd681776515f45d1c343a55cb
GaviriaAmaya <judamayaga@unal.edu.co>
2019-05-20 22:51:47 +0000

    [Ft] Safe division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    resu = 0
+    try:
+        resu = a / b
+    except ZeroDivisionError:
+        resu = None
+        return(None)
+    finally:
+        print("Inside result: {}".format(resu))
+    return(resu)
