c4b62e799814da01589ccd75dcc52f0c2d8e8968
GaviriaAmaya <judamayaga@unal.edu.co>
2019-05-22 03:58:01 +0000

    [Fix] Fixing privacy on square size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, __size):
+    def __init__(self, __size=0):
         if type(__size) is not int:
             raise TypeError("size must be an integer")
         if __size < 0:
