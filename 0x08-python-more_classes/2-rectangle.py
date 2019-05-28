ead2d10051c636075d44c7789d7258343cd2647c
GaviriaAmaya <judamayaga@unal.edu.co>
2019-05-28 05:18:30 +0000

    [FT] Add area and perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
@@ -27,3 +27,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        return self.height * self.width
+        
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (2 * self.height) + (2 * self.width)
