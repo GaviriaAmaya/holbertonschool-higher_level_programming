da1a01d523d02fd663d5964c063596903671b971
GaviriaAmaya <judamayaga@unal.edu.co>
2019-05-30 05:06:55 +0000

    [Feature] Apply class attribute that can change the repr. Lef t to cast into list

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 class Rectangle:
+
+    number_of_instances = 0
+    print_symbol = "#"
+
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -39,7 +44,10 @@ class Rectangle:
     def __str__(self):
         rep = ""
         for i in range(self.__height):
-            rep = "{}{}\n".format(rep, "#" * self.__width)
+            if self.__width <= 0:
+                rep = " \n"
+                break
+            rep = "{}{}\n".format(rep, str(self.print_symbol) * self.__width)
         return rep[:-1]
 
     def __repr__(self):
@@ -47,4 +55,4 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
-        
+        type(self).number_of_instances -= 1
