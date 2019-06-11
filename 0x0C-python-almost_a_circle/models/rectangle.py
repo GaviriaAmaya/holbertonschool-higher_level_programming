833b135187c3606cdd34fddcd8137071bf020e24
GaviriaAmaya <judamayaga@unal.edu.co>
2019-06-11 21:12:21 +0000

    [Fetaure] Add Class Rectangle thaat inherits Base method id. Define instances for size and position and getters an setters

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"Inheritation from Class Base"
+from models.base import Base
+
+
+class Rectangle(Base):
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        "Rectangle constructor"
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def height(self):
+        return __height.self
+
+    @property
+    def width(self):
+        return __width.self
+
+    @property
+    def x(self):
+        return __x.self
+
+    @property
+    def y(self):
+        return __y.self
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @x.setter
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
