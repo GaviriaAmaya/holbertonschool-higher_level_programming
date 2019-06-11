e0e95d531b1e991d9ff520104b72988420eb3e16
GaviriaAmaya <judamayaga@unal.edu.co>
2019-06-11 21:57:07 +0000

    [Fix]

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 class Base:
-    "Define Class Base"
+    """Define Class Base"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        "Constructor Definition"
+        """Constructor Definition"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = self.__nb_objects
+            self.id = Base.__nb_objects
