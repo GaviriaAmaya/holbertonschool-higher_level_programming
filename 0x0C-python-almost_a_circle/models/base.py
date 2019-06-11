59b058650650b099331ea0bf4183737e813ae662
GaviriaAmaya <judamayaga@unal.edu.co>
2019-06-11 20:31:02 +0000

    [Feature] Define class constructor. Create module Base and do some requirements

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Base:
+    'Define Class Base'
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        'Constructor Definition'
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
