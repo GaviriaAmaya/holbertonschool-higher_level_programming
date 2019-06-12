e1d36bb53ab8e737d5f313a8ae63567f7196c4c9
GaviriaAmaya <judamayaga@unal.edu.co>
2019-06-12 03:57:27 +0000

    [Feature] Implement args and kwargs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -98,15 +98,28 @@ class Rectangle(Base):
         """update - Updates, through args and kwargs method, the values of
            attributes. Are defined according the constructor order
         """
-        if len(args) == 0:
+        if args is None or len(args) == 0:
             for key in kwargs:
                 if key == "id":
-                    self.id = kwargs.get(key)
+                    self.id = kwargs[key]
                 if key == "width":
-                    self.width = kwargs.get(key)
+                    self.width = kwargs[key]
                 if key == "height":
-                    self.height = kwargs.get(key)
+                    self.height = kwargs[key]
                 if key == "x":
-                    self.x = kwargs.get(key)
+                    self.x = kwargs[key]
                 if key == "y":
-                    self.y = kwargs.get(key)
+                    self.y = kwargs[key]
+
+        else:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[i]
+                elif i == 1:
+                    self.width = args[i]
+                elif i == 2:
+                    self.height = args[i]
+                elif i == 3:
+                    self.x = args[i]
+                elif i == 4:
+                    self.y = args[i]
