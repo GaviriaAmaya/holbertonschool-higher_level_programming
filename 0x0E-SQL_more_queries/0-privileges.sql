e2be026f3f9184d5eff6eeb03fb90b9e6bde23b5
GaviriaAmaya <judamayaga@unal.edu.co>
2019-07-17 04:34:12 +0000

    [Fix] Correcting comments

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
---Script thaat lists privileges of the MySQL users on localhost server
+-- Script thaat lists privileges of the MySQL users on localhost server
 SHOW GRANTS FOR `user_0d_1`@`localhost`;
 SHOW GRANTS FOR `user_0d_2`@`localhost`;
