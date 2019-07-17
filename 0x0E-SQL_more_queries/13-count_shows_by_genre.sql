ee243c00d22eb7ee1fc7e3cba930eef6da472dad
GaviriaAmaya <judamayaga@unal.edu.co>
2019-07-17 04:54:40 +0000

    [

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,4 @@ FROM tv_genres
 INNER JOIN  tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_show_genres.show_id  IS NOT NULL
-GROUP BY tv_genres.name ORDER BY number_of_shows DE
+GROUP BY tv_genres.name ORDER BY number_of_shows DESC
