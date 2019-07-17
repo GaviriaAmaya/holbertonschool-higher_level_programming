3c10d9647173dced5510eb860928eae225e1b405
GaviriaAmaya <judamayaga@unal.edu.co>
2019-07-17 04:41:30 +0000

    [Feature Add 11 and 12]

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,7 @@
--- Lists shows that have genre
+-- Lists the shows that have no genre
 SELECT tv_shows.title ,tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON  tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
