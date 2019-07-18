c629cb1b9a4d7077b8eff46917fca6f565065575
GaviriaAmaya <judamayaga@unal.edu.co>
2019-07-18 04:58:45 +0000

    [Feature] Adding advanced tasks

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,5 @@
+-- Displays on Farenheit the average temperature
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,5 @@
+-- Lists the max temperature per state
+SELECT state, MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
+ORDER BY state
