8d670ed713795d517b21a3367be1faa9a69b375d
GaviriaAmaya <judamayaga@unal.edu.co>
2019-08-12 05:00:58 +0000

    [Fix] Save from injection other input classes

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,11 +6,10 @@ import MySQLdb
 
 if __name__ == "__main__":
 
-
-    dtb = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
-                          passwd=sys.argv[2], database=sys.argv[3])
+    dtb = MySQLdb.connect(host="localhost", user='%s' % sys.argv[1], port=3306,
+                          passwd='%s' % sys.argv[2],
+                          database='%s' % sys.argv[3])
     cur = dtb.cursor()
-
     cur.execute("SELECT * FROM states WHERE BINARY name = '%s' ORDER\
                 BY id ASC" % sys.argv[4])
     query_rows = cur.fetchall()
