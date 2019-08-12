452b8bb9ced98ee87058c6bf942e712887ca4c65
GaviriaAmaya <judamayaga@unal.edu.co>
2019-08-12 08:06:57 +0000

    [Fix] Syntax and unexisting cases

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-""
+"Prints all the cities according to the state id order"
 import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
 
-    dtb = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
-                          passwd=sys.argv[2], database=sys.argv[3])
-    cur = dtb.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM states\
-                INNER JOIN cities ON states.id = cities.state_id\
-                ORDER BY states.id ASC")
-    query_rows = cur.fetchall()
+    if (len(sys.argv) == 4):
+        dtb = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
+                              passwd=sys.argv[2], database=sys.argv[3])
+        cur = dtb.cursor()
+        cur.execute("SELECT cities.id, cities.name, states.name FROM states\
+                    INNER JOIN cities ON states.id = cities.state_id\
+                    ORDER BY states.id ASC")
 
-    for row in query_rows:
-        print(row)
+        query_rows = cur.fetchall()
 
-    cur.close()
-    dtb.close()
+        for row in query_rows:
+            print(row)
+
+        cur.close()
+        dtb.close()
