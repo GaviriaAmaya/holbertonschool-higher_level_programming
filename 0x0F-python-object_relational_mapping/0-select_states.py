a1de565c551f0532aaf93a9d6d0731672a49e374
GaviriaAmaya <judamayaga@unal.edu.co>
2019-08-12 03:16:32 +0000

    [Feature] Add Python script that selects from db with MySQLlb

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#Lists all the atates from the database
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+    if (len(sys.argv == 4)):
+        dtb = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
+                              passwd=sys.argv[2], database=sys.argv[3])
+    cur = db.cursor()
+    cur.execute(SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    dtb.close()
