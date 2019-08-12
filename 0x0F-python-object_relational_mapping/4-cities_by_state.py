4f5ce0b4bb2136de8e3546d5a197f436763d0746
GaviriaAmaya <judamayaga@unal.edu.co>
2019-08-12 08:42:23 +0000

    [Fix] Ordered by cities id

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         cur = dtb.cursor()
         cur.execute("SELECT cities.id, cities.name, states.name FROM states\
                     INNER JOIN cities ON states.id = cities.state_id\
-                    ORDER BY states.id ASC")
+                    ORDER BY cities.id ASC")
 
         query_rows = cur.fetchall()
 
