c1a447006d64ad97204ae30e24d1be7640661fdc
GaviriaAmaya <judamayaga@unal.edu.co>
2019-08-12 04:36:41 +0000

    [Fix] Case sensitive

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     dtb = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
                           passwd=sys.argv[2], database=sys.argv[3])
     cur = dtb.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER\
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{}' ORDER\
                 BY id ASC".format(sys.argv[4]))
     query_rows = cur.fetchall()
 
