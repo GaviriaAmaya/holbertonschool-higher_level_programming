f5336b461b4efa05de7007aed7f33e15e9237643
GaviriaAmaya <judamayaga@unal.edu.co>
2019-08-12 04:34:29 +0000

    [Fix] Format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
                 BY id ASC".format(sys.argv[4]))
     query_rows = cur.fetchall()
 
-    print(query_rows)
+    for row in query_rows:
+        print(row)
 
     cur.close()
     dtb.close()
