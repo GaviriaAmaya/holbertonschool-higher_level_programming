f429e33f2b07f44ccbabf1f0a42c1ce039bea182
GaviriaAmaya <judamayaga@unal.edu.co>
2019-08-21 04:57:55 +0000

    [Fix] Remove additional comment

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #Script that takes an URL ($1), send a GET request and displays the body of response (-L)
-#The response is based on a custom header variable, with 98 value
 curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
