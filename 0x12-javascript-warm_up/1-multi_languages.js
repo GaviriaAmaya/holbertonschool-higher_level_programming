ef6a5706373f5040d7418db82803b954456672b7
GaviriaAmaya <judamayaga@unal.edu.co>
2019-09-17 19:08:28 +0000

    [Fix] Unuse variables

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
-// Prints three constant var with concatenation. Semistandard doesn't allow single line var
-const JS = 'Javascript is amazing';
-const Py = 'Python is cool\n';
-const C = 'C is fun\n';
-console.log(C + Py + JS);
+// Prints Long string. Semistandard doesn't allow single line var
+console.log('C is fun\nPython is cool\nJavascript is awesome');
