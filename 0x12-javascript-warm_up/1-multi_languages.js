dff79b34c6490ed0bd2968712bcc008aa759fe4a
GaviriaAmaya <judamayaga@unal.edu.co>
2019-09-17 19:04:21 +0000

    [Feature] Add concatenated printing

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Prints three constant var with concatenation. Semistandard doesn't allow single line var
+const JS = 'Javascript is amazing';
+const Py = 'Python is cool\n';
+const C = 'C is fun\n';
+console.log(C + Py + JS);
