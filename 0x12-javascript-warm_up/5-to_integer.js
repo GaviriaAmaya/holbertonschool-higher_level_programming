d79c2619919be127e56e4f34eb483d514b96fe24
Juan Amaya G <47118161+GaviriaAmaya@users.noreply.github.com>
2019-09-18 00:31:10 -0500

    [Fix] Var name

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Cast the first argument received into a number, if is possible 
 const num = Number.parseInt(process.argv[2]);
-if (isNaN(number)) {
+if (isNaN(num)) {
   console.log('Not a number');
 } else {
   console.log('My number:', num);
