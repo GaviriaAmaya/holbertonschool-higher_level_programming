358078af734540c58d1516bdb24a0c576876d949
Juan Amaya G <47118161+GaviriaAmaya@users.noreply.github.com>
2019-09-18 00:22:49 -0500

    [Feature] Print n timesC is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-// 
+// Print n times a message
+const num = Number.parseInt(process.argv[2]);
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (const i = 0; i < number; i++) {
+    console.log('C is fun');
+  }
+}
