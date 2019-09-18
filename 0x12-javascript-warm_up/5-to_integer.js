290438325ad3ebad1420433aef32dfb844566b50
Juan Amaya G <47118161+GaviriaAmaya@users.noreply.github.com>
2019-09-17 23:03:27 -0500

    [Feature] Script that casts the first argument into number f its format is coincident

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
-// 
+// Cast the first argument received into a number, if is possible 
+const num = Number.parseInt(process.argv[2]);
+if (isNaN(number)) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', num);
+}
