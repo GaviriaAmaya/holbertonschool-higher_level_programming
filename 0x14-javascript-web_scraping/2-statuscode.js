43139755886e3f583251b825c8ab5392824111c7
GaviriaAmaya <judamayaga@unal.edu.co>
2019-09-24 20:14:02 +0000

    [Fix] Corrected method of status

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,9 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (error, response, body) {
+request(url, function (error, response) {
   if (error) {
     console.log(error);
   }
-  console.log('code: ', response.StatusCode);
+    console.log('code:', response.statusCode);
 });
