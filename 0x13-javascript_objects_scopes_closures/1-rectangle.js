fc3945fb9e2a34f895338d6e830f6979a28747ab
GaviriaAmaya <judamayaga@unal.edu.co>
2019-09-24 03:08:15 +0000

    [Feature] Add constructor and height and width attributes

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Create a Rectangle class on JS, with constructor defined
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
