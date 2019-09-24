384d9ef38cf0cf3b9591e4be91c96347f2319310
GaviriaAmaya <judamayaga@unal.edu.co>
2019-09-24 03:57:52 +0000

    [Feature] Add instance attributes rotate() and double()

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+// Rectangle with rotate and double instance methods. Based on printable R
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width))
+    }
+  }
+
+  rotate() {
+    let change = this.width;
+    this.width = this.height;
+    this.height = change;
+  }
+
+  double() {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
