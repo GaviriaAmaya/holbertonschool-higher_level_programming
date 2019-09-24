f4ad71cced0fd50d971b4f8aba06536884e4fd76
GaviriaAmaya <judamayaga@unal.edu.co>
2019-09-24 05:03:12 +0000

    [Fix] Semistandard styling

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 // Prints a list in reverse
 
 exports.esrever = function (list) {
-  let reverse = [];
+  const reverse = [];
   for (let lengther = (list.length) - 1; lengther >= 0; lengther--) {
     reverse.push(list[lengther]);
   }
