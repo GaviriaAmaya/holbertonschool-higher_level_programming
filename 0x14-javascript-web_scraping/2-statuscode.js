486d1aa40186aeaa9b39fe0a9b3a82834666502a
GaviriaAmaya <judamayaga@unal.edu.co>
2019-09-24 20:06:28 +0000

    [Feature] Add function that gets request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Get the request code
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  console.log('code: ', response.StatusCode);
+});
