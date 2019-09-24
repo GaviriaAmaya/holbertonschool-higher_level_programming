0d352c0284a4ab817146759bb23e2fb196ee3a63
GaviriaAmaya <judamayaga@unal.edu.co>
2019-09-24 03:44:44 +0000

    [Feature] Add graphic representation of Rectangle class

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// Define a rectangle with height and width. Printable
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
