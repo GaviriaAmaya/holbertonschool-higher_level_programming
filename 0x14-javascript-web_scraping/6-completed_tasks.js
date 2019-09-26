39afc47bd3303f73f8bfda9aab4af4f66e9a21e9
GaviriaAmaya <judamayaga@unal.edu.co>
2019-09-26 04:50:13 +0000

    [Feature] Add script that computes the number of tasks completed by user id

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// COmpleted!
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, function (error, response, body) {
+  const task = {};
+  if (error) {
+    return console.log(error);
+  } else {
+    for (let i = 0; i < JSON.parse(body).length; i++) {
+      if (JSON.parse(body)[i].completed) {
+	const count = JSON.parse(body)[i].userId;
+	task[count] = (task[count] || 0) + 1;
+      }
+    }
+  }
+  console.log(task);
+});
