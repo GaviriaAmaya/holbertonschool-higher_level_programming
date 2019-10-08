ba0954e8d5bf9426d612113fbb39bc1c6d9df3f3
GaviriaAmaya <judamayaga@unal.edu.co>
2019-10-08 14:52:54 +0000

    [Feature] JS function that adds to numbers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,2 +1,17 @@
 #!/usr/bin/node
-// 
+// Function that adds two integers
+
+let num1 = process.argv[2];
+let num2 = process.argv[3];
+
+if (process.argv <= 2) {
+  console.log(NaN);
+} else if (isNaN(num1) || isNaN(num2)) {
+  console.log(NaN);
+} else {
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
+
+  const result = num1 + num2;
+  console.log(result);
+}
