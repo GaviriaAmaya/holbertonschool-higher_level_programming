d1ff6ea05abfe2a851f9da451b68a3629f588c71
GaviriaAmaya <judamayaga@unal.edu.co>
2019-10-08 15:29:09 +0000

    [Fix] Semistandard documentation applied

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 // Function that adds two integers
 
-let a = process.argv[2];
-let b = process.argv[3];
+const a = process.argv[2];
+const b = process.argv[3];
 
-
-function add(a, b) {
+function add (a, b) {
   if (process.argv[3]) {
     return (parseInt(a)) + (parseInt(b));
   } else {
     return NaN;
-  } 
-};
+  }
+}
 
-  console.log(add(a, b));
+console.log(add(a, b));
