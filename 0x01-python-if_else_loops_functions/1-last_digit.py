44f25a5d9ee255225e2b8190460fcddfb0654514
GaviriaAmaya <judamayaga@unal.edu.co>
2020-02-23 00:30:29 -0500

    [Add] Define the last digit of a random number and compares it

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"Take a random number and defines the last number"
+import random
+number = random.randint(-10000, 10000)
+"Absolute value of number, to take the right module"
+if number < 0:
+    module = (number * -1) % 10
+else:
+    module = number % 10
+print("Last digit of {:d} is {:d} ".format(number, module), end="")
+
+if module > 5:
+    print("and is greater than 5")
+elif module in range(1, 6):
+    print("and is less than 6 and not 0")
+elif module == 0:
+    print("and is 0")
